7bd34a120eba7c4c6fe838850b4f912dce1d6d0e
Luciana Sugeta <lulusugeta@gmail.com>
2021-10-18 12:36:50 -0400

    commit tree 62dec9bf55aec58df03ee42ed632e04e61982549

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,18 @@
 // your Bomb code here!
+import React from 'react'
+
+class Bomb extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render() {
+        // debugger
+        return this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`
+    }
+}
+
+export default Bomb;
